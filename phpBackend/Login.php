7b05e4fc85c14b597a7d02bff5ad2a2f367abd8e
Maydic489 <74528891+Maydic489@users.noreply.github.com>
2024-08-07 17:43:08 +0700

    insert login time stamp data when user login in Thailand time zone

--- phpBackend/Login.php
+++ phpBackend/Login.php
@@ -19,6 +19,7 @@ if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         if ($row["password"] == $loginPass) {
             echo "login Success.<br>";
+            UpdateLoginTimeStamp($conn, $loginUser);
         } else {
             echo "Wrong Credentials.";
         }
@@ -28,4 +29,18 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 
+
+function UpdateLoginTimeStamp($conn, $loginUser)
+{
+    $sql = "SELECT id FROM users WHERE username = '" . $loginUser . "'";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    $id = $row["id"];
+
+    date_default_timezone_set('Asia/Bangkok');
+    $dateTime = date("Y-m-d H:i:s");
+
+    $sql = "INSERT login_history (user_id, login_time) VALUES ('$id', '$dateTime')";
+    $conn->query($sql);
+}
 ?>
